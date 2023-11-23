887d3b6a1b66e4646911c81277f8670569a2b554
JEEVA T <jeevasakthi118@gmail.com>
2023-11-23 17:43:20 +0530

    initial repo setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/posts/*" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+export default function () {
+  return (
+    <Grid container>
+      <Grid item>Directory</Grid>
+    </Grid>
+  );
+}
